074f0e26cfd9384bfc3774bf7f30583f28b87c1e
lippoaraujo <lippoaraujo.dev@gmail.com>
2020-12-24 00:16:46 -0300

    fix: error on db connec artisan command

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,11 +6,13 @@ use App\Traits\ApiResponser;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 // use Illuminate\Http\Response;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
 use Modules\System\Entities\Action;
 use Modules\System\Entities\Route;
 use Modules\System\Entities\User;
+use Throwable;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -37,49 +39,58 @@ class AuthServiceProvider extends ServiceProvider
         // Gate::define('owner', function(User $user, $entity) {
         //     return $user->id === $entity->user_id;
         // });
-
-        $routes = collect();
-        if(Schema::hasTable('routes')) {
-            $routes = Route::all();
+        try {
+            DB::connection()->getPdo();
+            $con = true;
+        } catch(Throwable $e) {
+            $con = false;
         }
 
-        foreach($routes as $route) {
+        if($con) {
 
-            $module = $route->module;
+            $routes = collect();
+            if(Schema::hasTable('routes')) {
+                $routes = Route::all();
+            }
 
-            Gate::define($route->name, function(User $user, $actionName = null) use($route, $module) {
+            foreach($routes as $route) {
 
-                if(!$module->active) {
-                    return Response::deny("module: {$module->name} was disabled unauthorized!");
-                }
+                $module = $route->module;
 
-                if($user->isTenant() && $user->isDefaultUsergroup()) {
-                    return Response::allow();
-                }
+                Gate::define($route->name, function(User $user, $actionName = null) use($route, $module) {
 
-                if(!empty($actionName)) {
-                    if($user->hasPermissionUserAction($route, $actionName) || $user->hasPermissionUsergroupAction($route, $actionName)) {
+                    if(!$module->active) {
+                        return Response::deny("module: {$module->name} was disabled unauthorized!");
+                    }
+
+                    if($user->isTenant() && $user->isDefaultUsergroup()) {
+                        return Response::allow();
+                    }
+
+                    if(!empty($actionName)) {
+                        if($user->hasPermissionUserAction($route, $actionName) || $user->hasPermissionUsergroupAction($route, $actionName)) {
+                            return Response::allow();
+                        } else {
+                            return Response::deny('action access unauthorized!');
+                        }
+                    }
+
+                    if($user->hasPermissionUserRoute($route) || $user->hasPermissionUsergroupRoute($route)) {
                         return Response::allow();
                     } else {
-                        return Response::deny('action access unauthorized!');
+                        return Response::deny('route access unauthorized!');
                     }
-                }
 
-                if($user->hasPermissionUserRoute($route) || $user->hasPermissionUsergroupRoute($route)) {
                     return Response::allow();
-                } else {
-                    return Response::deny('route access unauthorized!');
-                }
-
-                return Response::allow();
-            });
-        }
-
-        Gate::before(function(User $user) {
-            if($user->isAdmin()) {
-                return Response::allow();
+                });
             }
-        });
 
+            Gate::before(function(User $user) {
+                if($user->isAdmin()) {
+                    return Response::allow();
+                }
+            });
+
+        }
     }
 }
